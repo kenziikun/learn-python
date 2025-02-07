6d525529d0f40a57d2902eb2204bdbbf956064fe
kenziikun <agismubarok.uix.code@gmail.com>
2025-02-07 16:43:23 +0700

    learn python/if statement

---
+++ 1 jam/main.1.3.py
@@ -0,0 +1,36 @@
+# if = do some code only IF some condition is True, else do something else
+'''
+age = int(input("masukan usia kamu: "))
+
+if age >= 18 :
+  print("kamu bisa sign up sekarang")
+elif age < 0:
+  print("kamu belum lahir cuy")
+else :
+  print("maaf usia mu masih di bawah umur, tidak bisa sign up")
+'''
+
+'''
+response = input("would you like food? (Y/N):")
+
+if response == "Y":
+  print("you have some food")
+else:
+  print("sorry no food for you!")
+'''
+
+'''
+name = input("masukan nama kamu: ")
+
+if name == "":
+  print("kamu belum memasukan nama")
+else:
+  print(f"hello {name}")
+'''
+
+online = True
+
+if online:
+  print("the user is online")
+else:
+  print("the user is offline")
